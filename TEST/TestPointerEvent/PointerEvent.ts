648e03eae111898282a99c37762ffc76d191558e
DieErdnuss <92154252+DieErdnuss@users.noreply.github.com>
2022-04-16 09:22:21 +0200

    Pointer Tesst

--- TEST/TestPointerEvent/PointerEvent.ts
+++ TEST/TestPointerEvent/PointerEvent.ts
@@ -5,7 +5,7 @@ let checkbox: HTMLInputElement;
 function hndLoad(_event: Event) {
     button = <HTMLDivElement> document.getElementById("button");
     button.addEventListener("click", hndClick);
-    checkbox = <HTMLInputElement> document.getElementsByTagName(checkbox);
+    checkbox = <HTMLInputElement> document.getElementsByTagName("checkbox");
     checkbox.addEventListener("change", hndChange);
 }
 
