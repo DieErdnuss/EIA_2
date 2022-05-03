4a47550864768669bb94b869401e0028e35035e8
DieErdnuss <92154252+DieErdnuss@users.noreply.github.com>
2022-05-03 11:05:54 +0200

    steckbrief

--- L08.1_Canvas/canvas_Uebung.js
+++ L08.1_Canvas/canvas_Uebung.js
@@ -3,8 +3,25 @@ window.addEventListener("load", hndLoad);
 function hndLoad() {
     let canvas = document.querySelector("canvas");
     let crc2 = canvas.getContext("2d");
-    crc2.fillRect(50, 50, crc2.canvas.width, crc2.canvas.height);
-    crc2.fillStyle = "#FF0000";
+    canvas.addEventListener("click", hndClick);
+    crc2.fillStyle = "#f999";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    crc2.beginPath();
+    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+    crc2.stroke();
+    crc2.beginPath();
+    crc2.ellipse(20, 20, 5, 8, Math.PI / 4, 0.5, 1.5 * Math.PI);
+    crc2.stroke();
+    crc2.closePath();
+    crc2.beginPath();
+    crc2.moveTo(200, 20);
+    crc2.lineTo(260, 100);
+    crc2.lineTo(120, 80);
+    crc2.lineTo(200, 20);
+    crc2.stroke();
+    crc2.closePath();
+}
+function hndClick(_event) {
 }
 // canvas.height = window.innerHeight;
 // canvas.width = window.innerWidth;
--- L08.1_Canvas/canvas_Uebung.ts
+++ L08.1_Canvas/canvas_Uebung.ts
@@ -1,12 +1,33 @@
 window.addEventListener("load", hndLoad);
 
 
+
 function hndLoad(): void {
     let canvas: HTMLCanvasElement = document.querySelector("canvas");
     let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+    canvas.addEventListener("click", hndClick);
+    crc2.fillStyle = "#f999";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
 
-    crc2.fillRect(50, 50, crc2.canvas.width, crc2.canvas.height);
-    crc2.fillStyle = "#FF0000";
+    crc2.beginPath();
+    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+    crc2.stroke();
+    
+    crc2.beginPath();
+    crc2.ellipse(20, 20, 5, 8, Math.PI / 4, 0.5, 1.5 * Math.PI)
+    crc2.stroke();
+    crc2.closePath();
+
+    crc2.beginPath();
+    crc2.moveTo(200, 20);
+    crc2.lineTo(260, 100);
+    crc2.lineTo(120, 80);
+    crc2.lineTo(200, 20);
+    crc2.stroke();
+    crc2.closePath();
+}
+
+function hndClick(_event: Event): void {
 
 }
 
