1c0404f7c7555434f657a4e2915eba05b70f3ff7
DieErdnuss <92154252+DieErdnuss@users.noreply.github.com>
2022-05-23 14:59:00 +0200

    Classes

--- L09_Classes/Farm/FarmAnimals.ts
+++ L09_Classes/Farm/FarmAnimals.ts
@@ -15,6 +15,7 @@ namespace McDonaldFarm {
         food: string;
         foodAmount: number;
         sound: string;
+
         constructor(_type: string, _name: string, _food: string, _foodAmount: number, _sound: string) {
             this.type = _type;
             this.name = _name;
@@ -22,6 +23,7 @@ namespace McDonaldFarm {
             this.foodAmount = _foodAmount;
             this.sound = _sound;
         }
+        
         eat(): void {
             food = String(this.food);
 
